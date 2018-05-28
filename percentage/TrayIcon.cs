8038615e4fb7e81f19d6b4f9d8078ef88dd9344d
LoliMay <1404363070@qq.com>
2018-05-28 22:30:29 +0800

    initial

---
+++ percentage/TrayIcon.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace percentage
+{
+    class TrayIcon
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern bool DestroyIcon(IntPtr handle);
+
+        private const string iconFont = "Segoe UI";
+        private const int iconFontSize = 26;    // 带渲染文本的字体大小
+
+        private string batteryPercentage;
+        private NotifyIcon notifyIcon;
+
+        public TrayIcon()
+        {
+            ContextMenu contextMenu = new ContextMenu();
+            MenuItem menuItem = new MenuItem();
+
+            notifyIcon = new NotifyIcon();
+
+            // initialize contextMenu
+            contextMenu.MenuItems.AddRange(new MenuItem[] { menuItem });
+
+            // initialize menuItem
+            menuItem.Index = 0;
+            menuItem.Text = "退出";
+            menuItem.Click += new System.EventHandler(menuItem_Click);
+
+            notifyIcon.ContextMenu = contextMenu;
+
+            batteryPercentage = "?";
+
+            notifyIcon.Visible = true;
+
+            Timer timer = new Timer();
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Interval = 1000; // in miliseconds
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            PowerStatus powerStatus = SystemInformation.PowerStatus;
+            batteryPercentage = (powerStatus.BatteryLifePercent * 100).ToString();
+
+            using (Bitmap bitmap = new Bitmap(DrawText(batteryPercentage, new Font(iconFont, iconFontSize), Color.DarkOrange, Color.Transparent)))   // 背景色透明
+            {
+                System.IntPtr intPtr = bitmap.GetHicon();
+                try
+                {
+                    using (Icon icon = Icon.FromHandle(intPtr))
+                    {
+                        notifyIcon.Icon = icon;
+                        notifyIcon.Text = batteryPercentage + "%";
+                    }
+                }
+                finally
+                {
+                    DestroyIcon(intPtr);
+                }
+            }
+        }
+
+        /**
+         * 右键菜单退出按钮点击事件
+         */
+        private void menuItem_Click(object sender, EventArgs e)
+        {
+            notifyIcon.Visible = false;
+            notifyIcon.Dispose();
+            Application.Exit();
+        }
+
+        private Image DrawText(String text, Font font, Color textColor, Color backColor)
+        {
+            var textSize = GetImageSize(text, font);
+            Image image = new Bitmap(45, 45);       // 位图大小
+            using (Graphics graphics = Graphics.FromImage(image))
+            {
+                // 绘制背景
+                graphics.Clear(backColor);
+
+                // 文本笔刷
+                using (Brush textBrush = new SolidBrush(textColor))
+                {
+                    graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SystemDefault;
+                    graphics.DrawString(text, font, textBrush, 3, -2);   //渲染文本的偏移
+                    graphics.Save();
+                }
+            }
+
+            return image;
+        }
+
+        private static SizeF GetImageSize(string text, Font font)
+        {
+            using (Image image = new Bitmap(10, 10))
+            using (Graphics graphics = Graphics.FromImage(image))
+                return graphics.MeasureString(text, font);
+        }
+    }
+}
